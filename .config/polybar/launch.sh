e672d92717f0a2278b67dba1b082a5b7607e3ab9
AntoninoAdornetto <armaninollc@protonmail.com>
2024-01-06 15:50:37 -0500

    chore: polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+killall polybar
+
+polybar-msg cmd quit
+
+echo "---" | tee -a /tmp/polybar1.log
+polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
