86f5dfdf370a4ddf1751ea30c9d5d2f928b073dc
AntoninoAdornetto <armaninollc@protonmail.com>
2024-09-25 21:29:32 +0000

    fix: multiple issues after integration testing

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,10 +5,10 @@ echo ""
 
 echo "Enter git username:"
 read user_name
-git config --global user.name user_name
+git config --global user.name $user_name
 
 echo "Enter git email:"
 read email
-git config --global user.email email
+git config --global user.email $email
 
 git config --global init.defaultBranch master
