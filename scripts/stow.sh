b9a40afba1a8f2ab56388b7871e9e68435460f85
AntoninoAdornetto <armaninollc@protonmail.com>
2024-10-16 21:07:01 -0600

    fix: incorrect symlinks

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -3,9 +3,17 @@
 CONFIG_DIR="$HOME/.config"
 IMG_DIR="$HOME/Pictures"
 
+if [ ! -d "$IMG_DIR/icons" ];then
+    mkdir -p "$IMG_DIR/icons"
+fi
+
+if [ ! -d "$IMG_DIR/wallpapers" ];then
+    mkdir -p "$IMG_DIR/wallpapers"
+fi
+
 # Create symlinks with GNU stow
 stow --target="$CONFIG_DIR" .config
-stow --target="$IMG_DIR" wallpapers
-stow --target="$IMG_DIR" icons
+stow --target="$IMG_DIR/wallpapers" wallpapers
+stow --target="$IMG_DIR/icons" icons
 stow tmux
 stow zsh
