2ea0f1a037c53bb9c3c2383a74816b93d1e92df5
AntoninoAdornetto <armaninollc@protonmail.com>
2024-10-16 19:08:21 -0600

    style: use stow to build wallpaper dir

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 CONFIG_DIR="$HOME/.config"
+IMG_DIR="$HOME/Pictures"
 
 # Create symlinks with GNU stow
 stow --target="$CONFIG_DIR" .config
+stow --target="$IMG_DIR" wallpapers
 stow tmux
 stow zsh
